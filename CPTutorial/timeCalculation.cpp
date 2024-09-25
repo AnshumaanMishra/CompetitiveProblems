e5ee82950766a7a032fb9e0d655e26fefef1dc7f
Anshumaan Mishra <anshumaanmishra.24@kgpian.iitkgp.ac.in>
2024-09-25 19:00:56 +0530

    Syncing with Windows

--- CPTutorial/timeCalculation.cpp
+++ CPTutorial/timeCalculation.cpp
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include <iostream>
 #include <ctime>
 #include <algorithm>
 #include <vector>
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    vector<int> newVector(n, 0);
+    for(int i = 0; i < n; i++){
+        newVector[i] = n - i;
+    }
+
     auto start_time = clock();
+    sort(newVector.begin(), newVector.end());
     auto end_time = clock();
 
-    printf("%d", end_time - start_time);
+    cout << end_time - start_time << endl;
     return 0;
 }
