fc3ab2985d16fbc4ecddc11b8a2701b929caa167
AnshumaanMishra <anshumaanmishra.24@kgpian.iitkgp.ac.in>
2024-10-16 08:38:43 +0530

    CodeForces 1200 problems

--- CodeForces/tempCodeRunnerFile.cpp
+++ CodeForces/tempCodeRunnerFile.cpp
@@ -1,22 +1,10 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-bool canBeRepresented(int n, int k, int subVal){
-    if((n % 2 == 0) && (k % 2 != 0)){
-        return false;
-    }
-    if((n % 2 != 0) && (k % 2 == 0)){
-        return false;
-    }
-    else{
-        if((n - subVal == 0) || ((k == 2) && ((n - subVal) % 2 == 1))){
-            return true;
-        }
-        return canBeRepresented(n - subVal, k - 1, subVal - 2);
-    }
-}
-
-int main(){
-    cout << canBeRepresented(10, 2, 1) << endl;
-    return 0;
-}
+        //     cout << k << endl;
+        //     t--;
+        //     continue;
+        // }
+        // if(n == k){
+        //     cout << n + 1 << endl;
+        //     t--;
+        //     continue;
+        // }
